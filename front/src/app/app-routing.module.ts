02905ae66b9cd919732fe876f1b1968f6fe95201
Frederic Guyont <you@example.com>
2024-09-07 09:54:32 +0200

    ADD: frontend chat feature

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChatComponent } from './components/chat/chat.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'chat/:userId', component: ChatComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
